7b3449e23dde6f92ebf4987e0e5065449fcc4106
alitnp <69058159+alitnp@users.noreply.github.com>
2020-08-18 00:51:43 +0430

    Updates

---
+++ precache-manifest.fba3601df0af3501c19b9a62d678f42a.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdaa7d8102cd865b148cc2e009dee424",
+    "url": "/react-resume/index.html"
+  },
+  {
+    "revision": "fc702833488846abd536",
+    "url": "/react-resume/static/css/main.6389087d.chunk.css"
+  },
+  {
+    "revision": "d80405ce5b168be50fae",
+    "url": "/react-resume/static/js/2.3b131e4f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-resume/static/js/2.3b131e4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc702833488846abd536",
+    "url": "/react-resume/static/js/main.a9430120.chunk.js"
+  },
+  {
+    "revision": "b901f24cb805d391570f",
+    "url": "/react-resume/static/js/runtime-main.623af3ea.js"
+  },
+  {
+    "revision": "ac5f106c269cf41af6f8f3771aaccdb1",
+    "url": "/react-resume/static/media/csharp.ac5f106c.svg"
+  },
+  {
+    "revision": "47bba155efe91d0a3a7374a7f28da950",
+    "url": "/react-resume/static/media/css.47bba155.svg"
+  },
+  {
+    "revision": "45b942bf0ce4120438f2ad6a12351a5d",
+    "url": "/react-resume/static/media/dart.45b942bf.svg"
+  },
+  {
+    "revision": "09ef4e2342689abdb3d682f0fd6fdd6d",
+    "url": "/react-resume/static/media/flutter.09ef4e23.svg"
+  },
+  {
+    "revision": "0ccffb5f6bc0b06c69aa4edcf3e5f05a",
+    "url": "/react-resume/static/media/html5.0ccffb5f.jpg"
+  },
+  {
+    "revision": "c8fde740526ae73f2e64849331b996e0",
+    "url": "/react-resume/static/media/html5.c8fde740.svg"
+  },
+  {
+    "revision": "579211df75e21fc5971c18f76561470e",
+    "url": "/react-resume/static/media/javascript.579211df.svg"
+  },
+  {
+    "revision": "67cd350b6bb5030a2ce07bd1cdd5cf02",
+    "url": "/react-resume/static/media/react.67cd350b.svg"
+  },
+  {
+    "revision": "6fab8362c05bc927b2ccbe331d06bac9",
+    "url": "/react-resume/static/media/sql.6fab8362.svg"
+  },
+  {
+    "revision": "d425f017bf090ee51d65b482a1eeee59",
+    "url": "/react-resume/static/media/theme.d425f017.png"
+  }
+]);
